b5857543cab158ff0a6ea5cf8caf86d3746eb6f5
mogobanyamwaro <mogobanyamwaro@gmail.com>
2021-07-15 07:20:50 +0300

    added google analytics

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ export const featuredPortfolio = [
   {
     id: 1,
     title: 'Frontend app',
+    link: 'https://bestmenwatch.myshopify.com/',
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU',
   },
   {
