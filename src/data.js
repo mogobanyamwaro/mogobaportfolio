6ff7d6588d7aca6ffd8dd2b056ec34e72e65f387
mogobanyamwaro <mogobanyamwaro@gmail.com>
2021-07-25 14:10:39 +0300

    updated the featured part

--- src/data.js
+++ src/data.js
@@ -3,32 +3,32 @@ export const featuredPortfolio = [
     id: 1,
     title: 'Frontend app',
     link: 'https://bestmenwatch.myshopify.com/',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU',
+    img: 'https://images.pexels.com/photos/1151440/pexels-photo-1151440.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
   },
   {
     id: 2,
     title: 'Jumia E-commerce',
-    img: 'https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900',
+    img: 'https://images.pexels.com/photos/4452372/pexels-photo-4452372.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
   },
   {
     id: 3,
-    title: 'Social Media App',
-    img: 'https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900',
+    title: 'Todo List',
+    img: 'https://images.pexels.com/photos/1937337/pexels-photo-1937337.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
   },
   {
     id: 4,
     title: 'Dashboard',
-    img: 'https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900',
+    img: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGFzaGJvYXJkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
   },
   {
     id: 5,
-    title: 'Django Api',
-    img: 'https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900',
+    title: 'Expense calculator app',
+    img: 'https://images.unsplash.com/photo-1553729459-efe14ef6055d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZXhwZW5zZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
   },
   {
     id: 6,
-    title: 'Node Js',
-    img: 'https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900',
+    title: 'Facebook App',
+    img: 'https://images.unsplash.com/photo-1432888622747-4eb9a8efeb07?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZmFjZWJvb2t8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
   },
 ];
 
