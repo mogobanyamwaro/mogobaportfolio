9f5f46e18f96fa1d768e16fe4e6f85d47470c491
Engineer Mogoba <mogobanyamwaro@gmail.com>
2022-09-08 21:19:52 +0300

    feat: update portifolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,37 +1,20 @@
-import React, { useEffect, useRef } from 'react';
-import { init } from 'ityped';
+import React, { useEffect, useRef } from "react";
+import { init } from "ityped";
 
-import './Intro.scss';
+import "./Intro.scss";
 function Intro() {
-  const textRef = useRef();
-  useEffect(() => {
-    init(textRef.current, {
-      showCursor: true,
-      backDelay: 1500,
-      backSpeed: 60,
-      strings: [
-        'Frontend Developer',
-        'Backend Developer',
-        'Shopify Developer',
-        'Digital Marketer',
-      ],
-    });
-  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/man.jpg" alt="" />
+          <img src="assets/Man.jpg" alt="" />
         </div>
       </div>
       <div className="right">
         <div className="wrapper">
           <h2>Hello, I'm</h2>
           <h1>Douglas Mogoba </h1>
-          <h3>
-            Lead
-            <span ref={textRef}></span>
-          </h3>
+          <h3 style={{ color: "darkblue" }}>FullStack Developer</h3>
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt="" />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import Contact from '../components/contact/Contact';
-import Intro from '../components/intro/Intro';
-import Navbar from '../components/navbar/Navbar';
-import Portifolio from '../components/portifolio/Portifolio';
-import Testimonials from '../components/testimonials/Testimonials';
-import Work from '../components/works/Work';
-import Menu from '../components/menu/Menu';
-import Skills from '../components/skills/Skills';
-import Footer from '../components/footer/Footer';
+import { useState } from "react";
+import Contact from "../components/contact/Contact";
+import Intro from "../components/intro/Intro";
+import Navbar from "../components/navbar/Navbar";
+import Portifolio from "../components/portifolio/Portifolio";
+import Testimonials from "../components/testimonials/Testimonials";
+import Work from "../components/works/Work";
+import Menu from "../components/menu/Menu";
+import Skills from "../components/skills/Skills";
+import Footer from "../components/footer/Footer";
 
 function Home() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -20,7 +20,7 @@ function Home() {
         <Portifolio />
         <Skills />
         <Work />
-        {/* <Testimonials /> */}
+
         <Contact />
         <Footer />
       </section>
